63db0029fb207b94a1d18d55aae8bc066c92a37b
mjtheriot4 <mther16@lsu.edu>
2017-06-07 13:55:47 -0500

    init

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+  loopSlides();
+});
+var i = 0;
+function loopSlides() {
+  if (i < 3) {
+    $('div#text-container>div').eq(i).delay(2000).fadeIn(1000).delay(6000).fadeOut(1000, function() {loopSlides()});
+    // $('div#text-container>div').eq(i).delay(2000).animate({top: "+=50%"},1000,"swing").delay(6000).animate({top: "-=50%"},1000,"swing", function() {loopSlides()});
+    i++;
+  }
+  else {
+    i = 0;
+    loopSlides();
+  }
+}
